3d35dc14af2f0d796ab40c9587bc310dd6b9a823
shaikhsameer018856-code <shaikhsameer018856@gmail.com>
2026-01-06 21:37:23 +0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (58.78%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,33 @@
+class MyQueue {
+    Stack<Integer> a = new Stack<>();
+    Stack<Integer> b = new Stack<>();
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        a.push(x);
+    }
+    
+    public int pop() {
+        if (b.isEmpty()){
+            while(!a.isEmpty()){
+                b.push(a.pop());
+            }
+        }
+        return b.pop();
+    }
+    
+    public int peek() {
+        if (b.isEmpty()){
+            while (!a.isEmpty()){
+                b.push(a.pop());
+            }
+        }
+        return b.peek();
+    }
+    
+    public boolean empty() {
+        return a.isEmpty() && b.isEmpty();
+    }
+}
